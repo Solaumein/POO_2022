51fa58528d1a9a0419d2cfeb3e9ea62a4166a15f
onnig <brulez@insa-toulouse.fr>
2023-01-05 08:47:30 +0100

    Integration avec controller créée

---
+++ src/main/java/org/example/Controller/MainScreenController.java
@@ -0,0 +1,40 @@
+package org.example.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainScreenController {
+
+    @FXML
+    public Button connectButton;
+
+    public void connectButtonAction(ActionEvent event){
+        Stage mainStage = (Stage) connectButton.getScene().getWindow();
+        URL urlofFXML= null;
+        try {
+            urlofFXML = new File("src/main/java/org/example/GUI/MainScreen.fxml").toURI().toURL();
+            Parent root = FXMLLoader.load(urlofFXML);
+            mainStage.setTitle("Clavardage Entre Pote");
+            mainStage.setScene(new Scene(root));
+            mainStage.show();
+
+        } catch (MalformedURLException e) {
+            System.out.println("fichier manquant MainScreen.fxml");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
