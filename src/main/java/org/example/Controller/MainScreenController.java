4d17a5959f1cd9c1348e5d45d6e65f6325054f6b
onnig <brulez@insa-toulouse.fr>
2023-01-06 14:12:46 +0100

    affichage utilisateurs

--- src/main/java/org/example/Controller/MainScreenController.java
+++ src/main/java/org/example/Controller/MainScreenController.java
@@ -2,6 +2,8 @@ package org.example.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCode;
@@ -10,6 +12,9 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainScreenController {
 
@@ -20,7 +25,10 @@ public class MainScreenController {
     @FXML
     public VBox messageZone;
 
-    public void SendButtonAction(ActionEvent event){
+    @FXML
+    public VBox listWindow;
+
+    public void SendButtonAction(ActionEvent event) throws IOException {
 
         String message = textToSend.getText();
         Text messageBubble = new Text(message);
@@ -28,6 +36,19 @@ public class MainScreenController {
         messageBubble.setTextAlignment(TextAlignment.RIGHT);
         messageZone.getChildren().add(messageBubble);
         textToSend.clear();
+
+        FXMLLoader loader = new FXMLLoader((new File("src/main/java/org/example/GUI/ContactFrame.fxml").toURI().toURL()));
+        System.out.println(loader.getLocation());
+        Node n = loader.load();
+        listWindow.getChildren().add(n);
+
+        Node node = (Node)loader.getNamespace().get("contactFrame");
+
+// Look up the label element inside the node
+        Label label = (Label)node.lookup("#elementId");
+
+// Modify the label's text property
+        label.setText("New label text");
     }
 
     public void displayReceivedMessage(String message){
